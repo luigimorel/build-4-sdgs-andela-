72e8e05c9b59071059d572f1c10b120234bd2c94
morelmiles <morelmiles@gmail.com>
2020-04-13 11:35:14 +0300

    Added project dependencies

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,15 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  const impact = {};
+  const severeImpact = {}
+  const period = null
 
+  //Challenge 1
+  impact.currentlyInfected = reportedCases * 10;
+  severeImpact.currentlyInfected = reportedCases * 50;
+
+  const currentlyInfected = 
+  {
+    return data;
+  }
+};
 export default covid19ImpactEstimator;
