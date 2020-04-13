a7411002e59eceef72e3605b67cb3c228dc92a16
morelmiles <morelmiles@gmail.com>
2020-04-13 12:16:54 +0300

    Challenge one

--- src/estimator.js
+++ src/estimator.js
@@ -1,15 +1,23 @@
 const covid19ImpactEstimator = (data) => {
   const impact = {};
-  const severeImpact = {}
-  const period = null
+  const severeImpact = {};
+  let period;
 
   //Challenge 1
   impact.currentlyInfected = reportedCases * 10;
   severeImpact.currentlyInfected = reportedCases * 50;
 
-  const currentlyInfected = 
-  {
-    return data;
-  }
+  if (data.periodType === 'days')
+    period = 2 ** Math.trunc(data.timeToElapse / 3);
+  else if (data.periodType === 'weeks')
+    period = 2 ** Math.trunc((data.timeToElapse * 7) / 3);
+  else if (data.periodType === 'months')
+    period = 2 ** Math.trunc((data.timeToElapse * 30) / 3);
+
+  impact.infectionsByRequestedTime = impact.currentlyInfected * period;
+  severeImpact.infectionsByRequestedTime =
+    severeImpact.currentlyInfected * period;
+
+  return { data };
 };
 export default covid19ImpactEstimator;
